a27a7c701c4d35908ca9bb11fbc8f85728db7ad1
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-08-07 22:40:19 +0530

    add education section

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <Projects />
         <Education />
 
-        <ContactSection />
+        {/* <ContactSection /> */}
         <ScrollToTop />
       </main>
       <Footer />
