10e157648fae0d14189bb548e84f0bd991a4b7bb
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-08-07 22:30:11 +0530

    navbar and footer completed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "../css/card.scss";
+import "../css/globals.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import "../css/card.scss";
-import "../css/globals.scss";
 import Navbar from "@/components/Navbar";
 import ScrollToTop from "@/components/ScrollToTop";
 import Footer from "@/components/Footer";
@@ -11,18 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <html lang="en">
-        <body className={inter.className}>
-          {/* <ToastContainer /> */}
-          <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-            <Navbar />
-            {children}
-            <ScrollToTop />
-          </main>
-          <Footer />
-        </body>
-        {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
-      </html>
+      {/* <ToastContainer /> */}
+      <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <Navbar />
+        {/* {children} */}
+        <ScrollToTop />
+      </main>
+      <Footer />
     </>
   );
 }
