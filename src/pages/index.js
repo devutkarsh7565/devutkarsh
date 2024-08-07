9f5bcacd452f8761be2fd6198c467fd9fcc01bd8
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-08-07 22:32:09 +0530

    hero section completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,14 @@ export default function Home() {
       {/* <ToastContainer /> */}
       <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
         <Navbar />
-        {/* {children} */}
+        <HeroSection />
+        <AboutSection />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Education />
+
+        <ContactSection />
         <ScrollToTop />
       </main>
       <Footer />
